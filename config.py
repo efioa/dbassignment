cc946e9e69c656251ee2ecde709bf416e9afccff
aoife lennon <lennonaoife5@gmail.com>
2023-12-01 21:25:03 +0000

    first commit

--- config.py
+++ config.py
@@ -2,4 +2,6 @@ import os
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'not-set')
 
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite://db.sqlite3')
+#SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite://db.sqlite3')
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
